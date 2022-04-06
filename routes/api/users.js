ffff680d73ac373ee7c95f0c3cc560e3162e2556
Zakk Fast <zakkfastpro@gmail.com>
2022-04-06 16:58:23 -0700

    set up user creation for db and jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/');
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -32,13 +36,40 @@ router.post(
         res.status(400).json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
 
-      // Encrypt password
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
-      // Return JWT
+      const salt = await bcrypt.genSalt(10);
 
-      res.send('User Route');
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');

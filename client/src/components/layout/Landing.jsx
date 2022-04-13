94ec4e792f113ee879e6c086ebdff5c45684928a
Zakk Fast <zakkfastpro@gmail.com>
2022-04-13 14:56:01 -0700

    init commit

---
+++ client/src/components/layout/Landing.jsx
@@ -0,0 +1,25 @@
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Devvy</h1>
+          <p className="lead">
+            Create a profile and portfolio, share posts and get help from
+            others
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;

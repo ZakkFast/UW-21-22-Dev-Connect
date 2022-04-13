e308d8cbe2d0cd203090944fde0cafdbbe9b68b4
Zakk Fast <zakkfastpro@gmail.com>
2022-04-13 15:26:13 -0700

    set up react-router for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import React, { Fragment } from 'react'
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+        <Route exact path='/' element={ <Landing /> } />
+        </Routes>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;

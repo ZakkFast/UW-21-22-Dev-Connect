2fd8f14974ee19bf4dbc6045da21d9c386bff8a1
Zakk Fast <zakkfastpro@gmail.com>
2022-04-13 15:42:14 -0700

    fixed syntax error due to newer React ver

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import './App.css';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
@@ -12,6 +15,8 @@ const App = () => {
         <Navbar />
         <Routes>
         <Route exact path='/' element={ <Landing /> } />
+          <Route exact path='/register' element={ <Register />} />
+          <Route exact path='/login' element={ <Login />} />
         </Routes>
       </Fragment>
     </Router>

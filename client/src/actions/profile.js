45590f5b9629a3934f71dd7b75f4c43bd4a53d02
Zakk Fast <zakkfastpro@gmail.com>
2022-04-23 19:20:04 -0700

    added delete edu and delete exp

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -105,3 +105,41 @@ export const addEducation = (formData, navigate) => async (dispatch) => {
     });
   }
 };
+
+// Delete Exp
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await api.delete(`/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete Edu
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await api.delete(`/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};

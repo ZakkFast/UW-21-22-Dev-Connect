6767c66f17361674d9fc8045e0cc889a1a16515f
Zakk Fast <zakkfastpro@gmail.com>
2022-04-23 20:56:52 -0700

    Post reducer, actions and component built

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -63,6 +65,11 @@ const App = () => {
             <Route
               path="add-education"
               element={<PrivateRoute component={AddEducation} />}
+            />
+            <Route path="posts" element={<PrivateRoute component={Posts} />} />
+            <Route
+              path="posts/:id"
+              element={<PrivateRoute component={Post} />}
             />
           </Routes>
         </Fragment>
